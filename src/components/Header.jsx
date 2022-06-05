3653e4536b588770a2d436a3fda759190d4f49d4
alejandrog150 <58411057+alejandrog150@users.noreply.github.com>
2022-06-05 08:03:05 -0600

    nuevo encabezado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ function Header() {
   return (
     <>
       <h1 className="font-black text-3xl text-center md:w-2/3 mx-auto">
-        Seguimiento Pacientes {""}
+        Seguimiento de Pacientes {""}
         <span className="text-indigo-600">Veterinaria</span>
       </h1>
     </>
