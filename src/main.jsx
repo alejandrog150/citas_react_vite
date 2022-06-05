6d9ea6e01225a8a203a779deab473e3bf41a54cc
alejandrog150 <58411057+alejandrog150@users.noreply.github.com>
2022-06-05 07:49:57 -0600

    css/output.css

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 //import ReactDOM from 'react-dom'
 import App from './App'
-import './index.css'
-import './dist/output.css'
+//import './index.css'
+import './css/output.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
